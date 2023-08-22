1829bd47e37ff459382f2ded247c983c534cca76
Kosala Rathnayake <kosalamadawa@gmail.com>
2023-08-22 23:04:21 +0530

    Updated backend API domain

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,5 +1,5 @@
 export const getPrediction = async (story: string): Promise<APIResponse>  => {
-  const url = `http://18.138.61.248/api/v1/predict?text=${story}`;
+  const url = `https://storycraft.lol/api/v1/predict?text=${story}`;
   const headers = new Headers();
   headers.append("API-KEY", "6d3ff9a5-38a4-41f9-a7c6-9eeff989c531");
   const response = await fetch(encodeURI(url), {
